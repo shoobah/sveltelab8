72d96005c5c3c73297c3eabfd662d886b0c2615d
Anders Sjöberg <anders.sjoberg@calabrio.com>
2020-01-23 15:41:24 +0100

    Start

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { timer } from "rxjs";
+
+  var message = "-";
+
+  timer(0, 1000).subscribe(value => (message = Date()));
+</script>
+
+<style>
+  main {
+    /* text-align: center; */
+    padding: 1em;
+    max-width: 240px;
+    /* margin: 0 auto; */
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 2em;
+    font-weight: 100;
+    user-select: none;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1>{message}</h1>
+</main>
